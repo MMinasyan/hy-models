92ddeaf0e56366b22907e1ed600b6cc2161c7e80
Mkrtich Minasyan <mvminasyan.96@gmail.com>
2025-06-10 23:24:44 +0400

    Add version number to hy_models module

--- hy_models/__init__.py
+++ hy_models/__init__.py
@@ -2,6 +2,8 @@ from .configuration import ArtConfig, AutoEditConfig
 from .modeling_art import ArtForCausalLM
 from .modeling_autoedit import AutoEditForConditionalGeneration
 
+__version__ = "0.1.0"
+
 __all__ = [
     "ArtConfig",
     "AutoEditConfig",
